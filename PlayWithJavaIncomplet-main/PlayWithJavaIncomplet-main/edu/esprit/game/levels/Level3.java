69a6fa8a9eaa12294688ba8747ca502cae54c24a
nourhene dahmeni <130768686+Dahmeni-Nourhene@users.noreply.github.com>
2023-11-15 12:15:20 +0100

    Add files via upload

---
+++ PlayWithJavaIncomplet-main/PlayWithJavaIncomplet-main/edu/esprit/game/levels/Level3.java
@@ -0,0 +1,52 @@
+package edu.esprit.game.levels;
+
+import edu.esprit.game.models.Employee;
+import edu.esprit.game.utils.Data;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Level3 {
+	public static void main(String[] args) {
+		List<Employee> employees = Data.employees();
+
+		// TO DO 1: Retourner une chaîne de caractère qui contient tous les noms des employés
+		String names = employees.stream()
+				.map(Employee::getName)
+				.reduce("", (name1, name2) -> name1 + " " + name2);
+
+		// TO DO 2: Retourner une chaîne de caractère qui contient tous les noms des employés en majuscule séparés par #
+		String namesMajSplit = employees.stream()
+				.map(Employee::getName)
+				.map(String::toUpperCase)
+				.collect(Collectors.joining("#"));
+
+		// TO DO 3: Retourner un Set d'employés
+		Set<Employee> emps = employees.stream().collect(Collectors.toSet());
+
+		// TO DO 4: Retourner un TreeSet d'employés (tri par nom)
+		TreeSet<Employee> emps2 = employees.stream()
+				.collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Employee::getName))));
+
+		// TO DO 5: Retourner une Map qui regroupe les employés par salaire
+		Map<Integer, List<Employee>> map = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getSalary));
+
+		// TO DO 6: Retourner une Map qui regroupe les noms des employés par salaire
+		Map<Integer, String> mm = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getSalary, Collectors.mapping(Employee::getName, Collectors.joining(","))));
+
+		// TO DO 7: Retourner le min, max, average, sum, count des salaires
+		String s = employees.stream()
+				.collect(Collectors.summarizingInt(Employee::getSalary))
+				.toString();
+
+		System.out.println("TO DO 1: " + names);
+		System.out.println("TO DO 2: " + namesMajSplit);
+		System.out.println("TO DO 3: " + emps);
+		System.out.println("TO DO 4: " + emps2);
+		System.out.println("TO DO 5: " + map);
+		System.out.println("TO DO 6: " + mm);
+		System.out.println("TO DO 7: " + s);
+	}
+}
